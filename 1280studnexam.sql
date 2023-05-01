d6b21a17d5aa9fe4e4c49aac98b4665f54c7b563
anveshjarabani <anveshjarabani@gmail.com>
2023-04-30 21:10:09 -0700

    leetcode sql

---
+++ 1280studnexam.sql
@@ -0,0 +1,42 @@
+Create table If Not Exists Students (student_id int, student_name varchar(20));
+Create table If Not Exists Subjects (subject_name varchar(20));
+Create table If Not Exists Examinations (student_id int, subject_name varchar(20));
+
+Truncate table Students;
+insert into Students (student_id, student_name) values ('1', 'Alice');
+insert into Students (student_id, student_name) values ('2', 'Bob');
+insert into Students (student_id, student_name) values ('13', 'John');
+insert into Students (student_id, student_name) values ('6', 'Alex');
+Truncate table Subjects;
+insert into Subjects (subject_name) values ('Math');
+insert into Subjects (subject_name) values ('Physics');
+insert into Subjects (subject_name) values ('Programming');
+Truncate table Examinations;
+insert into Examinations (student_id, subject_name) values ('1', 'Math');
+insert into Examinations (student_id, subject_name) values ('1', 'Physics');
+insert into Examinations (student_id, subject_name) values ('1', 'Programming');
+insert into Examinations (student_id, subject_name) values ('2', 'Programming');
+insert into Examinations (student_id, subject_name) values ('1', 'Physics');
+insert into Examinations (student_id, subject_name) values ('1', 'Math');
+insert into Examinations (student_id, subject_name) values ('13', 'Math');
+insert into Examinations (student_id, subject_name) values ('13', 'Programming');
+insert into Examinations (student_id, subject_name) values ('13', 'Physics');
+insert into Examinations (student_id, subject_name) values ('2', 'Math');
+insert into Examinations (student_id, subject_name) values ('1', 'Math');
+
+
+SELECT e.student_id,e.subject_name,count(e.subject_name) as attended_exams from examinations e
+group by e.student_id,e.subject_name;
+
+
+with cte as
+    (select * from subjects sb cross join students s order by student_id,sb.subject_name
+    )
+select cte.*,
+case when (attended_exams) is NULL then 0
+else attended_exams end as attended_exams from cte
+left join (SELECT e.student_id,e.subject_name,count(e.subject_name) as attended_exams from examinations e
+group by e.student_id,e.subject_name) s on
+cte.student_id= s.student_id and 
+cte.subject_name=s.subject_name
+ORDER BY student_id
