2972e0dd027643dcd1e69690b5243b7df0ed97f1
anveshjarabani <anveshjarabani@gmail.com>
2023-05-12 15:58:38 -0700

    updated code

--- enterprise_bombuild.sql
+++ enterprise_bombuild.sql
@@ -4,7 +4,7 @@
 with RECURSIVE cte as 
     (select *, cast(`QTY` as FLOAT) as TOPLEVEL
     from st_bm_br_bom
-    where `MATERIAL`='CY-210257'
+    where `MATERIAL`='TEST-PN'
     union all
     (SELECT s.*, cast(s.QTY as FLOAT)*cte.TOPLEVEL as TOPLEVEL
      from st_bm_br_bom s
@@ -14,3 +14,23 @@ SELECT * from cte;
 where `COMPONENT` = 'CY-103720' OR `MATERIAL` = 'CY-103720';
 
 select distinct `TOP LEVEL` from cy_adj_yt;
+
+
+
+WITH RECURSIVE cte AS (
+        SELECT
+            *,
+            CAST(`QTY` AS FLOAT) AS TOPLEVEL
+        FROM st_bm_br_bom
+        WHERE `MATERIAL` = 'TEST-PN'
+        UNION ALL
+        SELECT
+            s.*,
+            CAST(s.QTY AS FLOAT) * cte.TOPLEVEL AS TOPLEVEL
+        FROM st_bm_br_bom s
+            JOIN cte ON cte.`COMPONENT` = s.`MATERIAL`
+    )
+SELECT c1.*, c2.*
+FROM cte c1, cte c2
+WHERE
+    c1.`COMPONENT` = c2.`COMPONENT`;
